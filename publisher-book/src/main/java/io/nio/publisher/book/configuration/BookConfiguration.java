c5c9441b853a8e68ed3164d76a005942a7727ad8
open-repository <soranam.parthasarathy@gmail.com>
2022-03-30 13:13:17 +0530

    Adding eureka server and client

---
+++ publisher-book/src/main/java/io/nio/publisher/book/configuration/BookConfiguration.java
@@ -0,0 +1,17 @@
+package io.nio.publisher.book.configuration;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class BookConfiguration {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate httpClient(){
+        return new RestTemplate();
+
+    }
+}
---
+++ publisher-discovery/src/test/java/io/nio/publisher/discovery/PublisherDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nio.publisher.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublisherDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
